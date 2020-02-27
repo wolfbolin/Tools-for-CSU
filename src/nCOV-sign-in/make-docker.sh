3c17765388aab9a21ba51f8899431cbcf7b12755
wolfbolin <wolfbolin@foxmail.com>
2020-02-27 16:44:37 +0800

    Use docker

---
+++ src/nCOV-sign-in/make-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+docker image prune -f
+docker stop ncov_sign
+docker rm ncov_sign
+echo -e "\033[5;36mOrz 旧容器(镜像)已清理\033[0m"
+
+time_now=$(date "+%m%d%H")
+docker build -f Dockerfile --tag ncov_sign:"${time_now}" .
+echo -e "\033[5;36mOrz 镜像重建完成\033[0m"
+
+docker run -itd \
+	--restart always \
+	--name ncov_sign\
+	ncov_sign:"${time_now}"
+echo -e "\033[5;36mOrz 镜像启动完成\033[0m"
+docker ps -a
+docker logs ncov_sign -f
